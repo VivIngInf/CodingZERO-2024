bfc65235b42d32f5819accfa2f2bede8635f44c1
KoganeNoShin <63775826+KoganeNoShin@users.noreply.github.com>
2024-09-24 23:26:24 +0200

    Add files via upload

--- Presentazioni/Lezione 4 - Corona Diego/CifrarioDiCesare.c
+++ Presentazioni/Lezione 4 - Corona Diego/CifrarioDiCesare.c
@@ -14,21 +14,14 @@ int main() {
     // Cifrario
     for(int i = 0; i < strlen(scritta); i++) {
         if (scritta[i] >= 'a' && scritta[i] <= 'z') {
-            scritta[i] = ((scritta[i] - 'a' + chiave) % 26) + 'a'; // Gestione delle lettere minuscole
+            scritta[i] = ((scritta[i] - 'a' + chiave) % 26) + 'a';
         } else if (scritta[i] >= 'A' && scritta[i] <= 'Z') {
-            scritta[i] = ((scritta[i] - 'A' + chiave) % 26) + 'A'; // Gestione delle lettere maiuscole
+            scritta[i] = ((scritta[i] - 'A' + chiave) % 26) + 'A';
         }
     }
 
     // Output
     printf("Stringa cifrata: %s\n", scritta);
 
-    return 0;
-}
-
-#include <string.h>
-int main(){
-    // Tipi di Array
-    char stringa[] = "Hello";
     return 0;
 }
